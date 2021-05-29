ab5814838faff61dae62d94b109c9f91c4bbe0c2
Darkslayer07 <shubhmittal297@gmail.com>
2021-05-29 17:56:33 +0530

    Main file commit 01

--- main.cpp
+++ main.cpp
@@ -8,14 +8,6 @@ sf::RenderWindow window(vm,"My first SFML test",sf::Style::Default);
 
 int main()
 {
-    sf::RectangleShape rect(sf::Vector2f(500.0f,300.0f));
-    rect.setFillColor(sf::Color::Yellow);
-    rect.setPosition(viewSize.x/2,viewSize.y/2);
-    rect.setOrigin(sf::Vector2f(rect.getSize().x/2,rect.getSize().y/2));
-    sf::CircleShape circle(100.0f);
-    circle.setFillColor(sf::Color::Green);
-    circle.setPosition(sf::Vector2f(viewSize.x/2,viewSize.y/2));
-    circle.setOrigin(sf::Vector2f(circle.getRadius(),circle.getRadius()));
     while(window.isOpen())
     {
         sf::Event event;
@@ -25,7 +17,6 @@ int main()
                 window.close();
         }
         window.clear(sf::Color::Red);
-        window.draw(rect);
         window.display();
     }
     
