ce5d70d9717f41e10ffe876857ed1fb68ebc9f1c
Darkslayer07 <shubhmittal297@gmail.com>
2021-05-30 03:24:01 +0530

    adding sprites commit 01

--- main.cpp
+++ main.cpp
@@ -4,29 +4,21 @@
 sf::Vector2f viewSize(1024,780);
 sf::VideoMode vm(viewSize.x,viewSize.y);
 sf::RenderWindow window(vm,"My first SFML test",sf::Style::Default);
+sf::Texture skyTexture;
+sf::Sprite skySprite;
 
-
+void init()
+{
+    skyTexture.loadFromFile("Assets/graphics/sky.png");
+    skySprite.setTexture(skyTexture);
+}
+void draw()
+{
+    window.draw(skySprite);
+}
 int main()
 {
-    sf::RectangleShape rect(sf::Vector2f(500.0f,300.0f));
-    rect.setFillColor(sf::Color::Yellow);
-    rect.setPosition(viewSize.x/2,viewSize.y/2);
-    rect.setOrigin(sf::Vector2f(rect.getSize().x/2,rect.getSize().y/2));
-    ////////////////////////////////////////////////////////////////////
-    sf::CircleShape circle(100.0f);
-    circle.setFillColor(sf::Color::Green);
-    circle.setPosition(sf::Vector2f(viewSize.x/2,viewSize.y/2));
-    circle.setOrigin(sf::Vector2f(circle.getRadius(),circle.getRadius()));
-    ////////////////////////////////////////////////////////////////////
-    sf::ConvexShape tri;
-    tri.setPointCount(4);
-    tri.setPoint(0, sf::Vector2f(-100, 0));
-    tri.setPoint(1, sf::Vector2f(0, -100));
-    tri.setPoint(2, sf::Vector2f(100, 0));
-    tri.setPoint(3, sf::Vector2f(0, 100));
-    tri.setFillColor(sf::Color(128, 0, 128, 255));
-    tri.setPosition(viewSize.x / 2, viewSize.y / 2);
-
+    init();
     while(window.isOpen())
     {
         sf::Event event;
@@ -36,9 +28,7 @@ int main()
                 window.close();
         }
         window.clear(sf::Color::Red);
-        window.draw(rect);
-        window.draw(circle);
-        window.draw(tri);
+        draw();
         window.display();
     }
     
