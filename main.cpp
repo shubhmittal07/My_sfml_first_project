72190f07cdbfb82732d5a8ceaffbc48d33a3efcc
Darkslayer07 <shubhmittal297@gmail.com>
2021-05-30 14:10:45 +0530

    Adding sprites commit 02--Dynamic resizing of sprites

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-sf::Vector2f viewSize(1024,780);
+sf::Vector2f viewSize(1024,768);
 sf::VideoMode vm(viewSize.x,viewSize.y);
 sf::RenderWindow window(vm,"My first SFML test",sf::Style::Default);
 sf::Texture skyTexture;
@@ -11,6 +11,10 @@ void init()
 {
     skyTexture.loadFromFile("Assets/graphics/sky.png");
     skySprite.setTexture(skyTexture);
+    skySprite.setScale(
+        viewSize.x/skySprite.getLocalBounds().width,
+        viewSize.y/skySprite.getLocalBounds().height);
+        //dynamic way to set the size of background sprites
 }
 void draw()
 {
@@ -27,7 +31,7 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        window.clear(sf::Color::Red);
+        window.clear(sf::Color::White);
         draw();
         window.display();
     }
